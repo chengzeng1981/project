02f7541cc87a914e13933e3dc28a501eed4bfa00
cloud-init created default user <udacity@suse.we2f5u0zqlsuveovql3qqalqec.ax.internal.cloudapp.net>
2025-03-24 09:07:44 +0000

    Initial commit

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["net-ssh",[">= 2.6.5","< 7.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"bfda3c63ecb2265e28d02d495b5625c667585dd8214eb6c177f66b44bac6b2ba","vagrant_version":"2.3.2"}
